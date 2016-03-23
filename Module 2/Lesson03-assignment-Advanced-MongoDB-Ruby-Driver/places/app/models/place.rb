caa9aba24e5460d8df07a74bddff74c16d04fe85
Rafa C <rafasantcont@yahoo.com.br>
2016-03-22 21:57:30 -0300

    Implement a class method called load_all that will bulk load a JSON document with places information into the places collection.

--- Module 2/Lesson03-assignment-Advanced-MongoDB-Ruby-Driver/places/app/models/place.rb
+++ Module 2/Lesson03-assignment-Advanced-MongoDB-Ruby-Driver/places/app/models/place.rb
@@ -8,4 +8,10 @@ class Place
   def self.collection
     self.mongo_client['places']
   end
+
+  def self.load_all file_path
+    file=File.read(file_path)
+    hash = JSON.parse(file)
+    self.collection.insert_many(hash)
+  end
 end
