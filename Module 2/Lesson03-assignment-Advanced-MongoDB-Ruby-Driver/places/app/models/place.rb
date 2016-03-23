7e39f7eafcba85962683dae4e8a97ee2e96f1047
Rafa C <rafasantcont@yahoo.com.br>
2016-03-22 22:27:23 -0300

    Implement read/write attributes in Place.

--- Module 2/Lesson03-assignment-Advanced-MongoDB-Ruby-Driver/places/app/models/place.rb
+++ Module 2/Lesson03-assignment-Advanced-MongoDB-Ruby-Driver/places/app/models/place.rb
@@ -1,6 +1,21 @@
 class Place
   include Mongoid::Document
 
+  attr_accessor :id, :formatted_address, :location, :address_components
+
+  def initialize params
+    @id = params[:_id].to_s
+    @formatted_address = params[:formatted_address]
+    @location = Point.new(params[:geometry][:geolocation])
+
+    @address_components = []
+    if !params[:address_components].nil?
+      params[:address_components].each {
+        |a| @address_components << AddressComponent.new(a)
+      }
+    end
+  end
+
   def self.mongo_client
     Mongoid::Clients.default
   end
