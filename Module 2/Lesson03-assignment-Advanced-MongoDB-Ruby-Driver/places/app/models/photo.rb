357e8ff43928a3a2876b0aaca10cf3b887c00b4a
Rafa C <rafasantcont@yahoo.com.br>
2016-03-29 23:24:26 -0300

    Add an initialize method in the Photo class.

--- Module 2/Lesson03-assignment-Advanced-MongoDB-Ruby-Driver/places/app/models/photo.rb
+++ Module 2/Lesson03-assignment-Advanced-MongoDB-Ruby-Driver/places/app/models/photo.rb
@@ -2,6 +2,12 @@ class Photo
   attr_accessor :id, :location
   attr_writer :contents
 
+  def initialize params = {}
+    @id = !params[:_id].nil? ? params[:_id].to_s : nil
+    @location = (params[:metadata] && params[:metadata][:location]) ? Point.new(params[:metadata][:location]) : nil
+    @place = (params[:metadata] && params[:metadata][:place]) ? params[:metadata][:place] : nil
+  end
+
   def self.mongo_client
     Mongoid::Clients.default
   end
